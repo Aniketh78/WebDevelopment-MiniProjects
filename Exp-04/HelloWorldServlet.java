6723f34692ea04dc2d03e50fa50dfea35da0123b
89p13 <231501014@rajalakshmi.edu.in>
2025-05-26 22:01:07 +0530

    Create HelloWorldServlet.java

---
+++ Exp-04/HelloWorldServlet.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, World!</h2>");
+        out.println("</body></html>");
+    }
+}
